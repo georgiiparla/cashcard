830f099d138e1d33d7342051b7c1a124ff5108d0
Georgii Parla <85189118+georgiiparla@users.noreply.github.com>
2025-06-11 02:54:21 +0200

    implement sorting and pagination

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.net.URI;
 
 import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -39,8 +44,13 @@ class CashCardController {
 		return ResponseEntity.created(locationOfNewCashCard).build();
 	}
 
-	@GetMapping()
-	private ResponseEntity<Iterable<CashCard>> findAll() {
-		return ResponseEntity.ok(cashCardRepository.findAll());
+	@GetMapping
+	private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+		Page<CashCard> page = cashCardRepository.findAll(
+				PageRequest.of(
+						pageable.getPageNumber(),
+						pageable.getPageSize(),
+						pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
+		return ResponseEntity.ok(page.getContent());
 	}
 }
--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,6 +1,7 @@
 package example.cashcard;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
 
-interface CashCardRepository extends CrudRepository<CashCard, Long> {
+interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
